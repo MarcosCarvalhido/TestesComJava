0618894befb11d7876e2d697cdfe881f18f65422
Marcos Carvalhido Rodrigues <marcos.carvalhido@al.infnet.edu.br>
2023-10-17 18:19:00 -0300

    Testes de Variaveis

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String mensagem = "Hello";
+        boolean verdade = true;
+        int numero = 1;
+        char letra = 'n';
+        double virvulas = 12.13;
+
+        System.out.println(mensagem + " " + verdade + " " + numero + " " + letra + " " + virvulas );
+
+    }
+}
